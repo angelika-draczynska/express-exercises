708dac0793b4bfd731329a49c5552299097da288
Angelika Draczynska <nayrissa@outlook.com>
2020-01-12 21:14:20 +0100

    add handlebars

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.show = (name) => {
-      res.sendFile(path.join(__dirname + `/views/${name}`));
-    };
-    next();
-  });
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
   app.use(express.static(path.join(__dirname + '/images')));
   
   app.use(express.static(__dirname));
 
   app.get('/user/', (req, res) => {
-    res.show('forbidden.html');
+    res.render('forbidden');
   });
 
   app.get('/', (req, res) => {
-    res.show('home.html');
+    res.render('home');
   });
-  app.get('/home', (req, res) => {
-    res.show('home.html');
-  });
+
   app.get('/about', (req, res) => {
-    res.show('about.html');
+    res.render('about');
+  });
+
+  app.get('/hello/:name', (req, res) => {
+    res.render('hello', { name: req.params.name });
   });
 
   app.use((req, res) => {
     res.status(404).show('404.html');
   });
-
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
